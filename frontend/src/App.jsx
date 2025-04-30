79d6710f5bd8fae586cd7b2d1a6fdf6b9066bab1
Diluka Chathuramka <159243058+DilukaChathuramka@users.noreply.github.com>
2025-04-30 22:12:40 +0530

    app.jsx helprequest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+//app.jsx
 
 function App() {
   return (
