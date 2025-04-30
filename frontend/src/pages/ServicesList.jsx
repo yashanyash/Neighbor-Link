01fe090d18ac4c4c641568610223415fae7ad3ba
Himaya Wijethunga <it22563996@my.sliit.lk>
2025-04-30 22:04:39 +0530

    update new change

--- frontend/src/pages/ServicesList.jsx
+++ frontend/src/pages/ServicesList.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-//update
+//update new 
 const ServicesList = () => {
   const [services, setServices] = useState([]);
   const [loading, setLoading] = useState(true);
