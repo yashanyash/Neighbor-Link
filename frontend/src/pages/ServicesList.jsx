c51053030e53cf39a1767d25bd839f925d59b53f
master <IT22575630@my.sliit.lk>
2025-04-30 22:00:34 +0530

    change new

--- frontend/src/pages/ServicesList.jsx
+++ frontend/src/pages/ServicesList.jsx
@@ -32,7 +32,7 @@ const ServicesList = () => {
       setLoading(false);
     }
   };
-
+//Update new
   const handleDelete = async (id) => {
     if (window.confirm('Are you sure you want to delete this service?')) {
       try {
